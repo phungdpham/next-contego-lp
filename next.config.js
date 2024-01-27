015753965613f85e7a5584fc2ba3661f24acc2ad
Phung Pham <phungpham@Phungs-MacBook-Pro-2.local>
2024-01-27 03:27:26 -0600

    redirect to theme page

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/contego-hygiene-liner',
+        destination: '/themes/hygiene-liner-1',
+        permanent: true,
+      },
+    ]
+  },
+}
